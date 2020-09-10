3edc7f6053a664460f96fa84aa5aa9eef4a4200b
artyuhvladislav <workto22@gmail.com>
2020-09-10 19:19:22 +0300

    fix: fixing task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return array.length !== 0 ? array.sort( (a, b) => a -b)[0] : 0
+  return array === undefined || array.length === 0 ? 0 : array.sort( (a, b) => a -b)[0]
 }
 
 exports.max = function max (array) {
-  return array.length !== 0 ? array.sort( (a, b) => a -b)[array.length - 1] : 0
+  return array === undefined  || array.length === 0 ? 0 : array.sort( (a, b) => a -b)[array.length - 1]
 }
 
 exports.avg = function avg (array) {
-  return array.length !== 0 ? array.reduce((cur, sum) => cur + sum) / array.length : 0
+  return array === undefined || array.length === 0 ? 0 : array.reduce((cur, sum) => cur + sum) / array.length 
 }
